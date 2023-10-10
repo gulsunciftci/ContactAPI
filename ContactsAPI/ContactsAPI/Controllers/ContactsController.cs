8daf4785a80b68635ec69e103a4e5baef54aeb30
Gulsun Ciftci <ciftcigulsun8@gmail.com>
2023-10-10 20:10:02 +0300

    controllera ekleme yapıldı

--- ContactsAPI/ContactsAPI/Controllers/ContactsController.cs
+++ ContactsAPI/ContactsAPI/Controllers/ContactsController.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ContactsAPI.Controllers
 {
-	[Route("api/[controller]")]
+	[Route("api/[controller]/[action]")]
 	[ApiController]
 	public class ContactsController : ControllerBase
 	{
